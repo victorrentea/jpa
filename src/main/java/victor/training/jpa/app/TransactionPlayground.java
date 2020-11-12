11af8faf8f430d105680b667f1e31fa7a1ca14e1
Victor Rentea <victorrentea@gmail.com>
2020-11-12 16:47:41 +0200

    for bidirectional relationships ALWAYS take care to assign the OWNER side of the two.

--- src/main/java/victor/training/jpa/app/TransactionPlayground.java
+++ src/main/java/victor/training/jpa/app/TransactionPlayground.java
@@ -12,8 +12,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import victor.training.jpa.app.domain.entity.ErrorLog;
+import victor.training.jpa.app.domain.entity.Subject;
 import victor.training.jpa.app.domain.entity.Teacher;
 import victor.training.jpa.app.repo.ErrorLogRepo;
+import victor.training.jpa.app.repo.SubjectRepo;
 import victor.training.jpa.app.repo.TeacherRepo;
 
 import javax.persistence.EntityManager;
@@ -30,12 +32,22 @@ public class TransactionPlayground {
    private final EntityManager entityManager; // JPA
    private final ErrorLogRepo errorLogRepo;
    private final TeacherRepo teacherRepo; // Spring Data JPA
+   private final SubjectRepo subjectRepo;
 
    @Transactional
    public void firstTransaction() {
       log.debug("Halo!");
       ErrorLog logError = errorLogRepo.save(new ErrorLog("Spring Data"));
       log.debug(logError.getId().toString());
+
+      /// ------------
+      Teacher teacher = new Teacher("Tavi");
+      teacherRepo.save(teacher);
+
+      Subject subject = new Subject();
+      teacher.getHeldSubjects().add(subject);
+      subject.setHolderTeacher(teacher);
+      subjectRepo.save(subject);
    }
 
    @Transactional
@@ -44,15 +56,17 @@ public class TransactionPlayground {
       ErrorLog errorLog = errorLogRepo.findById(1L).get();
       log.debug("Before the change");
 
-      teacherRepo.save(new Teacher("Tavi"));
-
       errorLog.setMessage("CHANGE");
-//      log.debug(errorLogRepo.findByMessageLike("%HANG%").toString());
-
-//      teacherRepo.flush(); // typically a code smell.
-      // useful: before calling a stored procedure or before using JDBCTemplate or MyBatis
-
+      log.debug(errorLogRepo.findByMessageLike("%HANG%").toString());
       errorLog.setMessage("BACK");
       log.debug("Is the update sent ABOVE this line ?");
+
+      // -------
+      List<Teacher> teachers = teacherRepo.findAll();
+      for (Teacher teacher : teachers) {
+         log.debug("Teacher {} teaching {}", teacher, teacher.getHeldSubjects());
+      }
+
+
    }
 }
