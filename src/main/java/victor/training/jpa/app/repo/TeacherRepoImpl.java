9f0269635291934aaf995953250ab4c745c554f9
Victor Rentea <victorrentea@gmail.com>
2020-11-13 12:20:18 +0200

    5: Query DSL

--- src/main/java/victor/training/jpa/app/repo/TeacherRepoImpl.java
+++ src/main/java/victor/training/jpa/app/repo/TeacherRepoImpl.java
@@ -10,14 +10,20 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import org.apache.commons.lang3.StringUtils;
 
+import victor.training.jpa.app.domain.entity.QTeacher;
 import victor.training.jpa.app.domain.entity.Teacher;
 import victor.training.jpa.app.domain.entity.Teacher.Grade;
 import victor.training.jpa.app.domain.entity.Teacher_;
 import victor.training.jpa.app.domain.entity.TeachingActivity;
 import victor.training.jpa.app.facade.dto.ActivitySearchCriteria;
 import victor.training.jpa.app.facade.dto.TeacherSearchCriteria;
+
+import static victor.training.jpa.app.domain.entity.QTeacher.teacher;
 
 public class TeacherRepoImpl implements TeacherRepoCustom {
 
@@ -47,34 +53,61 @@ public class TeacherRepoImpl implements TeacherRepoCustom {
 //      }
 //      return query.getResultList();
 //   }
+
+
+
    @Override
    public List<Teacher> search(TeacherSearchCriteria searchCriteria) {
-      CriteriaBuilder cb = em.getCriteriaBuilder();
-      CriteriaQuery<Teacher> criteriaQuery = cb.createQuery(Teacher.class);
-      Root<Teacher> root = criteriaQuery.from(Teacher.class);
 
-      List<Predicate> predicates = new ArrayList<>();
+      JPAQuery<?> query = new JPAQuery<Void>(em);
+      BooleanExpression pred = Expressions.TRUE;
+
 
       if (searchCriteria.grade != null) {
-         //  jpql += " AND t.grade = :grade ";
-         predicates.add(namePredicate(searchCriteria, cb, root));
+         pred = pred.and(teacher.grade.eq(searchCriteria.grade));
       }
+         if (searchCriteria.name != null) {
+            pred = pred.and(teacher.name.upper().like("%" + searchCriteria.name.toUpperCase() + "%"));
+         }
 
-      if (searchCriteria.name != null) {
-         //  jpql += " AND t.name = :grade ";
-         predicates.add(getLike(searchCriteria, cb, root));
-      }
-
-      criteriaQuery.select(root).where(cb.and(predicates.toArray(new Predicate[0])));
-
-      return em.createQuery(criteriaQuery).getResultList();
+      return query.select(teacher)
+             .from(teacher)
+             .where(pred)
+             .fetchAll()
+             .fetch();
    }
 
-   private Predicate getLike(TeacherSearchCriteria searchCriteria, CriteriaBuilder cb, Root<Teacher> root) {
-      return cb.like(cb.upper(root.get(Teacher_.name)), "%" + searchCriteria.name.toUpperCase() + "%");
-   }
 
-   private Predicate namePredicate(TeacherSearchCriteria searchCriteria, CriteriaBuilder cb, Root<Teacher> root) {
-      return cb.equal(root.get(Teacher_.grade), searchCriteria.grade);
-   }
+//   @Override
+//   public List<Teacher> search(TeacherSearchCriteria searchCriteria) {
+//      CriteriaBuilder cb = em.getCriteriaBuilder();
+//      CriteriaQuery<Teacher> criteriaQuery = cb.createQuery(Teacher.class);
+//      Root<Teacher> root = criteriaQuery.from(Teacher.class);
+//
+//      List<Predicate> predicates = new ArrayList<>();
+//
+//      if (searchCriteria.grade != null) {
+//         //  jpql += " AND t.grade = :grade ";
+//         predicates.add(namePredicate(searchCriteria, cb, root));
+//      }
+//
+//      if (searchCriteria.name != null) {
+//         //  jpql += " AND t.name = :grade ";
+//         predicates.add(getLike(searchCriteria, cb, root));
+//      }
+//
+//      criteriaQuery.select(root).where(cb.and(predicates.toArray(new Predicate[0])));
+//
+//      return em.createQuery(criteriaQuery).getResultList();
+//   }
+//
+//   private Predicate getLike(TeacherSearchCriteria searchCriteria, CriteriaBuilder cb, Root<Teacher> root) {
+//      return cb.like(cb.upper(root.get(Teacher_.name)), "%" + searchCriteria.name.toUpperCase() + "%");
+//   }
+//
+//   private Predicate namePredicate(TeacherSearchCriteria searchCriteria, CriteriaBuilder cb, Root<Teacher> root) {
+//      return cb.equal(root.get(Teacher_.grade), searchCriteria.grade);
+//   }
+//}
+
 }
