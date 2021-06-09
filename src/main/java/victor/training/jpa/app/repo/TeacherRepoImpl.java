2761208701628e52181014ce036e37efd657a125
Victor Rentea <victorrentea@gmail.com>
2021-06-09 18:36:29 +0300

    cleanup

--- src/main/java/victor/training/jpa/app/repo/TeacherRepoImpl.java
+++ src/main/java/victor/training/jpa/app/repo/TeacherRepoImpl.java
@@ -20,6 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
 import static victor.training.jpa.app.domain.entity.QTeacher.teacher;
 
 public class TeacherRepoImpl implements TeacherRepoCustom {
