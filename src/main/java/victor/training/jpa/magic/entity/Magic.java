2650b6977158ca46d09d14fefaffada75270ceec
Victor Rentea <victorrentea@gmail.com>
2021-07-08 10:40:55 +0300

    Batch start

--- src/main/java/victor/training/jpa/magic/entity/Magic.java
+++ src/main/java/victor/training/jpa/magic/entity/Magic.java
@@ -7,26 +7,26 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.domain.DomainEvents;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import victor.training.jpa.magic.event.DomainEvent;
 import victor.training.jpa.magic.event.MagicHappenedEvent;
 import victor.training.jpa.magic.event.publisher.EventPublisherHolder;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Transient;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static lombok.AccessLevel.PRIVATE;
 
 @Entity
 @Data
 @NoArgsConstructor(access = PRIVATE)
-//@EntityListeners(AuditingEntityListener.class) or global via orm.xml
+//@EntityListeners(AuditingEntityListener.class)// or global via orm.xml
 public class Magic {
    @Id
    @GeneratedValue
@@ -44,6 +44,26 @@ public class Magic {
    private String lastModifiedBy;
    @LastModifiedDate
    private LocalDateTime lastModifiedTime;
+
+   String stringuriCsv;
+
+//   @Transient
+//   List<String> stringuri;
+
+
+//   public List<String> getStringuri() {
+//      return Arrays.stream(stringuriCsv.split(",")).collect(Collectors.toList());
+//   }
+//
+//   @PostLoad
+//public void split() {
+//   csv --> stirngurl
+//}
+//   @PrePersist
+//   @PreUpdate
+//   public void method() {
+//      stringuriCsv = String.join(",", stringuri);
+//   }
 
    public Magic(String name) {
       this.name = name;
