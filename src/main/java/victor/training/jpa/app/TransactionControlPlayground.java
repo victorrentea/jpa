d3cf19f747395ac7a1e4b106deba8e53433455ea
Victor Rentea <victorrentea@gmail.com>
2021-10-04 16:47:06 +0300

    Propagare de tranzactii

--- src/main/java/victor/training/jpa/app/TransactionControlPlayground.java
+++ src/main/java/victor/training/jpa/app/TransactionControlPlayground.java
@@ -3,6 +3,7 @@ package victor.training.jpa.app;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,19 +23,30 @@ public class TransactionControlPlayground {
     private final JdbcTemplate jdbc;
     private final TeacherRepo teacherRepo;
     private final ErrorLogRepo repo;
+    private final AltService altService;
 
-
-    @Transactional
+    @Transactional // deschide TX noua pentru ca inca nu exista una deschisa pe th curent
     public void firstTransaction() {
         log.debug("Function Begin");
-        jdbc.update("INSERT INTO ERROR_LOG(ID, MESSAGE) VALUES ( -1,'AUTO_COMMIT mode' )");
-        jdbc.update("INSERT INTO ERROR_LOG(ID, MESSAGE) VALUES ( -2,'null' )");
-//        repo.save(new ErrorLog("CEVA AUTOCOMMIT"));
+
+        repo.save(new ErrorLog("Fara @Tr in jur"));
+        altService.metodaDeBiz();
+
         log.debug("Function End");
     }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void secondTransaction() {
         log.debug("Halo2!");
+    }
+}
+@Component
+@RequiredArgsConstructor
+class AltService {
+    private final ErrorLogRepo repo;
+
+    @Transactional // prelua Tx deschisa la linia 28
+    public void metodaDeBiz() {
+        repo.save(new ErrorLog("#sieu"));
     }
 }
