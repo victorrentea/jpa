f470d55e553da5242cef2a872b1277bfd0c3dd23
Victor Rentea <victorrentea@gmail.com>
2021-10-05 09:31:30 +0300

    Start day 2

--- src/main/java/victor/training/jpa/app/TransactionControlPlayground.java
+++ src/main/java/victor/training/jpa/app/TransactionControlPlayground.java
@@ -2,16 +2,25 @@ package victor.training.jpa.app;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.config.AopConfigUtils;
+import org.springframework.aop.framework.AopContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 import victor.training.jpa.app.domain.entity.ErrorLog;
 import victor.training.jpa.app.repo.ErrorLogRepo;
 import victor.training.jpa.app.repo.TeacherRepo;
 
 import javax.persistence.EntityManager;
+import javax.transaction.UserTransaction;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Slf4j
 @Service
@@ -22,6 +31,10 @@ public class TransactionControlPlayground {
     private final TeacherRepo teacherRepo;
     private final ErrorLogRepo repo;
     private final AltService altService;
+//    @Autowired
+//    private PlatformTransactionManager txManager;
+//    private final TransactionTemplate newTx;
+    UserTransaction ut;
 
     @Transactional // deschide TX noua pentru ca inca nu exista una deschisa pe th curent
     public ErrorLog firstTransaction() {
@@ -33,7 +46,11 @@ public class TransactionControlPlayground {
             altService.metodaDeBiz();
         } catch (Exception ex) {
             // ce exceptie?!
-            secondTransaction(ex.getMessage());
+            ex.printStackTrace();
+            altService.secondTransaction(ex.getMessage());
+
+//            newTx.executeWithoutResult(s-> {
+//            });
         }
 
 //        em.flush();
@@ -47,13 +64,22 @@ public class TransactionControlPlayground {
         repo.save(new ErrorLog("Eroare: " + message));
         log.debug("Halo2!");
     }
+//    @Autowired
+//    private AltService altService;
+
 }
+@Slf4j
 @Component
 @RequiredArgsConstructor
 class AltService {
     private final ErrorLogRepo repo;
 
-    @Transactional // prelua Tx deschisa la linia 28
+    @Transactional(propagation = Propagation.REQUIRES_NEW) // NOT_SUPPORTED also works here
+    public void secondTransaction(String message) {
+        repo.save(new ErrorLog("Eroare: " + message));
+        log.debug("Halo2!");
+    }
+//    @Transactional // prelua Tx deschisa la linia 28
     public void metodaDeBiz() {
         repo.save(new ErrorLog("#sieu"));
         throw new RuntimeException("BUBA");
