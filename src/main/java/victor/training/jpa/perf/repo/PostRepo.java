bec903cb7f13bcd6a047c6c04397e9e85b3484f6
Victor Rentea <victorrentea@gmail.com>
2022-01-17 22:56:49 +0200

    driving query working

--- src/main/java/victor/training/jpa/perf/repo/PostRepo.java
+++ src/main/java/victor/training/jpa/perf/repo/PostRepo.java
@@ -1,6 +1,5 @@
 package victor.training.jpa.perf.repo;
 
-import org.hibernate.annotations.Parent;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,8 +7,17 @@ import org.springframework.data.jpa.repository.Query;
 import victor.training.jpa.perf.entity.Post;
 
 import java.util.List;
+import java.util.Set;
 
 public interface PostRepo extends JpaRepository<Post, Long>, PostRepoCustom {
    @Query("SELECT p FROM Post p WHERE p.title LIKE ?1")
    Page<Post> findByTitleLike(String namePart, Pageable page);
+
+   // TODO delete
+   @Query("SELECT p.id FROM Post p WHERE p.title LIKE ?1")
+   Page<Long> findIdsByTitleLike(String namePart, Pageable page);
+
+   // TODO delete
+   @Query("SELECT p FROM Post p LEFT JOIN FETCH p.user LEFT join FETCH p.comments c LEFT JOIN FETCH c.user where p.id IN (?1)")
+   Set<Post> fetchForSearch(List<Long> idList);
 }
