b653687fcad2fc4fefb2fc222f7b44f539ab199d
victorrentea <victorrentea@gmail.com>
2022-06-09 13:25:45 +0300

    delete prev db

---
+++ readme.md
@@ -0,0 +1,9 @@
+# JPA Training 
+
+### Assignment
+First, start up the database by running `StartDatabase.java`. You can then connect to it from your favourite DB manager, using:
+- connection string `jdbc:h2:tcp://localhost:9092/~/test`
+- user `sa`
+- password `sa`
+- driver H2
+
--- src/main/java/victor/training/jpa/StartDatabase.java
+++ src/main/java/victor/training/jpa/StartDatabase.java
@@ -7,31 +7,25 @@ import java.sql.SQLException;
 
 public class StartDatabase {
 	public static void main(String[] args) throws SQLException {
-		deletePreviousDBContents();
-
 		System.out.println("Started DB...");
-		//hsqldb does not support Nested Transactions (REQUIRES_NEW).
-//		org.hsqldb.server.Server.main("--database.0 mem:test --dbname.0 test".split(" "));
+		System.out.println("Connecting to 'jdbc:h2:tcp://localhost:9092/~/test' will auto-create a database file 'test.mv.db' in user home (~)...");
 
-		// H2 does :)
+		deletePreviousDB();
+
+		// Allow auto-creating new databases on disk at first connection
 		org.h2.tools.Server.createTcpServer("-ifNotExists").start();
-
 	}
 
-	private static void deletePreviousDBContents() {
-		File userHomeFolder = new File(System.getProperty("user.home"));
-		if (!userHomeFolder.isDirectory()) {
-			throw new IllegalArgumentException("Could not locate userHome");
-		}
-		File databaseFile = new File(userHomeFolder, "test.mv.db");
-		System.out.println("Database file: " + databaseFile.getAbsolutePath());
-		if (databaseFile.isFile()) {
-			System.out.println("Deleting previous db contents...");
-			boolean ok = databaseFile.delete();
-			if (!ok) {
-				System.err.println("Could not delete database file: in use?");
+	private static void deletePreviousDB() {
+		File dbFile = new File(System.getProperty("user.home"), "test.mv.db");
+		System.out.println("Db file: " +dbFile.getAbsolutePath());
+
+		if (dbFile.isFile()) {
+			boolean r = dbFile.delete();
+			if (!r) {
+				System.err.println("Could not delete previous db content!");
 			} else {
-				System.out.println("SUCCESS");
+				System.out.println("Previous DB contents wiped out");
 			}
 		}
 	}
