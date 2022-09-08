b7d64c6a5a0bec02c46ce4f5f017f150fa726728
victorrentea <victorrentea@gmail.com>
2022-09-08 15:21:51 +0300

    cleanup

--- src/main/java/victor/training/jpa/app/TransactionPlayground.java
+++ src/main/java/victor/training/jpa/app/TransactionPlayground.java
@@ -27,34 +27,15 @@ public class TransactionPlayground {
 
 
     public void firstTransaction() {
-
-//        Connection conn = ds.getConnection();
-//        conn.prepareStatement("SQL NATIV")
-
-
         log.debug("Function Begin");
-
-        // natural key:
-        //        String id = "CUI RO1412515";
-        //        String id = "<fileName>-<retryNo>-<lineNo>";
-        // PRO: cheile inseamna ceva. Au meaning.
-        // PRO: poti genera PK FARA sa intrebi baza. > mai rapid un pic la inserturi masive (cica)
-        // CONS: mai dai si PK violation
-        // CONS: performanta mai slaba in DB INDECSII merg mai prost
-
-        String id = UUID.randomUUID().toString(); // urat dar unic
-        // PRO: poate fi generat de oricine, oriunde.
-
         ErrorLog entity = new ErrorLog("Halo!");
-        repo.save(entity); // daca generezi ID-uri cu strategy=IDENTITY => pentru a obtine acest ID nou, hib trebuie sa TRIMITA INSERTUL in DB. ATUNCI PE LOC. >> nu poate face batching,
-        System.out.println("Intotdeauna dupa SAVE, chiar DACA" +
-                           " NU VEZI INSERTUL DUCANDU_SE IN BAZA, entitatii tale i s-a setat ID = <seq>" + entity.getId());
-
-        repo.save(new ErrorLog(null));
+        repo.save(entity);
+        System.out.println("Intotdeauna dupa SAVE id = " + entity.getId());
+        System.out.println("chiar DACA NU VEZI INSERTUL DUCANDU_SE IN BAZA!! Huh!?!");
 
         log.debug("Function End");
-        jdbc.update("INSERT INTO TEACHER(ID) VALUES (HIBERNATE_SEQUENCE.nextval)");
     }
+//        jdbc.update("INSERT INTO TEACHER(ID) VALUES (HIBERNATE_SEQUENCE.nextval)");
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void secondTransaction() {
