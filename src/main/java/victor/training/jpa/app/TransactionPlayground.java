8cf3c0e9dc3b1f35984c07182d29db4468b39ba1
victorrentea <victorrentea@gmail.com>
2022-09-09 10:00:40 +0300

    apeluri locale de met nu trec prin @Transactional (proxy in general)

--- src/main/java/victor/training/jpa/app/TransactionPlayground.java
+++ src/main/java/victor/training/jpa/app/TransactionPlayground.java
@@ -2,13 +2,14 @@ package victor.training.jpa.app;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import victor.training.jpa.app.entity.*;
 import victor.training.jpa.app.repo.ErrorLogRepo;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 
 @Slf4j
 @Service
@@ -19,13 +20,29 @@ public class TransactionPlayground {
     private final Other other;
 
     @Transactional
-    public void firstTransaction() throws IOException {
+    public void firstTransaction()  {
         log.debug("Function Begin");
-        repo.save(new ErrorLog("mesaj"));
-        if (true) {
-            throw new IOException("Orice exceptie  (validate, ... conn timeout)");
+        try {
+            bizAdanc();
+        } catch (Exception e) {
+            saveError(e);
+            throw e;
         }
         log.debug("Function End");
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW) // NU MERGE CA NU TRECE PRIN PROXY!!
+    // un apel de metoda in aceeasi clasa (local) NU TRECE PRIN PROXY.
+    public void saveError(Exception e) {
+        repo.save(new ErrorLog("EROARE VALEU: " + e.getMessage()));
+    }
+
+    private void bizAdanc() {
+        repo.save(new ErrorLog("BIZ mesaj"));
+
+        if (true) {
+            throw new UncheckedIOException(new IOException("Orice exceptie  (validate, ... conn timeout)"));
+        }
     }
 
     @Transactional
