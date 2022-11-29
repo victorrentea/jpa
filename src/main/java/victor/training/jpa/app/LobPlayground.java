eaebac2e6f769fa647c6d9539aa9798262664793
Victor Rentea <victorrentea@gmail.com>
2022-11-29 09:15:44 +0200

    prep conn starvation. prep lob

--- src/main/java/victor/training/jpa/app/LobPlayground.java
+++ src/main/java/victor/training/jpa/app/LobPlayground.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import victor.training.jpa.app.entity.Teacher;
 import victor.training.jpa.app.entity.TeacherDetails;
 import victor.training.jpa.app.repo.TeacherDetailsRepo;
+import victor.training.jpa.app.repo.TeacherRepo;
 
 import java.io.*;
 import java.sql.SQLException;
@@ -16,10 +18,11 @@ import java.sql.SQLException;
 @Slf4j
 public class LobPlayground {
    private final TeacherDetailsRepo repo;
-   private Long id;
+   private final TeacherRepo teacherRepo;
+   private Long teacherId;
 
    @Transactional
-   public void uploadLargeClob() throws IOException {
+   public Long uploadLargeClob() throws IOException {
       File file = new File("pom.xml");
       if (!file.isFile()) {
          throw new IllegalArgumentException("File not found: " + file.getAbsolutePath());
@@ -27,13 +30,20 @@ public class LobPlayground {
       FileReader fileReader = new FileReader(file);
 //      Clob clob = ClobProxy.generateProxy(fileReader, file.length());
       String allContents = IOUtils.toString(fileReader);
-      TeacherDetails entity = new TeacherDetails().setCv(allContents);
-      id = repo.save(entity).getId();
+      TeacherDetails details = new TeacherDetails().setCv(allContents);
+      teacherId = teacherRepo.save(new Teacher().setDetails(details)).getId();
+      return repo.save(details).getId();
    }
    @Transactional
-   public void downloadLargeClob() throws IOException, SQLException {
-      TeacherDetails teacherDetails = repo.findById(id).get();
+   public void downloadLargeClob(Long uploadId) throws IOException, SQLException {
+      TeacherDetails teacherDetails = repo.findById(uploadId).get();
       System.out.println("Loaded teacher details");
       IOUtils.write(teacherDetails.getCv(), new OutputStreamWriter(System.out));
+   }
+
+   public void getTeacherAutoLoadsTheClob() {
+      Teacher teacher = teacherRepo.findOneById(teacherId);
+      System.out.println("Loaded teacher");
+      System.out.println("CV = " + teacher.getDetails().getCv());
    }
 }
