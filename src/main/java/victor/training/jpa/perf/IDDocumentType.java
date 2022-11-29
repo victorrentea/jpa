a74482f228129cdf3573d3af91a4fc83f1d28618
Victor Rentea <victorrentea@gmail.com>
2022-11-29 15:27:04 +0200

    IDDocumentType data is now also kept in the 2nd level cache!

--- src/main/java/victor/training/jpa/perf/IDDocumentType.java
+++ src/main/java/victor/training/jpa/perf/IDDocumentType.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,6 +14,7 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 public class IDDocumentType {
   @Id
   @GeneratedValue
