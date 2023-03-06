d59691e196658df4428f9d65e77b5aa3e0bbc6eb
Victor Rentea <victorrentea@gmail.com>
2023-03-06 12:44:50 +0200

    perf pitfall : REST (non-DB network calls) withing a @Transactional method in a high TPS system.

--- src/main/java/victor/training/jpa/app/TransactionPlayground.java
+++ src/main/java/victor/training/jpa/app/TransactionPlayground.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.web.client.RestTemplate;
 import victor.training.jpa.app.entity.ErrorLog;
 import victor.training.jpa.app.repo.ErrorLogRepo;
 import victor.training.jpa.app.repo.TeacherRepo;
@@ -21,7 +23,9 @@ public class TransactionPlayground {
     private final ErrorLogRepo errorLogRepo;
     private final OtherClass otherClass;
 
-    @Transactional // this proxy does NOT see any exception (it gets swallowed)
+    @Transactional(/*timeout = 5*/
+//    isolation = Isolation.READ_UNCOMMITTED // end of the world-> use Mongo instead!
+    ) // this proxy does NOT see any exception (it gets swallowed)
     public void firstTransaction() {
         log.debug("Function Begin talking to a proxied reference: " + otherClass.getClass());
         errorLogRepo.save(new ErrorLog("Halo!") );// this is executed AFTER the method end.
@@ -39,6 +43,21 @@ public class TransactionPlayground {
         }
         errorLogRepo.saveAndFlush(new ErrorLog("You will see me in the logs as INSERT, but I will never be commited."));
         log.warn("Function End");
+    }
+
+    @Transactional
+    public void badForPerformance() {
+//        new RestTemplate().getForObject() from another API-> why are we doing REST stuff in a @Transaction?!! =>
+                    // if the call takes 2 sec => you just paralyzed 1 connection from the pool (max=20) for 2 sec
+                // you took 5% of your system for 2 sec. if 20 such calls  to badForPerformance arrive,
+        // your system is unresponsive for 2 sec.
+        // WSDL  call
+//        WebClinet..
+        // in grafana you will see a large connection acquisition time
+
+        errorLogRepo.save(new ErrorLog("Stuff1"));
+        errorLogRepo.save(new ErrorLog("Stuff2"));
+
     }
 }
 @RequiredArgsConstructor
