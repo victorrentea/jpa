182958368ed8af9deb1995e72523ddee67deda41
Victor Rentea <victorrentea@gmail.com>
2023-03-08 11:52:10 +0200

    using transaction hooks to run code after the flush + commit of a @Transactional

--- src/main/java/victor/training/jpa/app/RichEntity.java
+++ src/main/java/victor/training/jpa/app/RichEntity.java
@@ -1,11 +1,15 @@
 package victor.training.jpa.app;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 import victor.training.jpa.app.entity.Subject;
 import victor.training.jpa.app.entity.Teacher;
 import victor.training.jpa.app.entity.Teacher.Status;
@@ -24,20 +28,18 @@ private final SubjectRepo subjectRepo;
   @EventListener(ApplicationStartedEvent.class)
   public void insertData() {
     log.info("START");
-    Teacher teacher = new Teacher().setName("Me")
+    Teacher teacher = new Teacher().setName("Meeee")
             .approve("jdoe"); // Domain Driven ubiquitout language < human words
-//            .setStatus(Status.APPROVED)
-//            .setApprovalUser("jdoe")
-//            .setApprovalDate(LocalDate.now());
     Subject subject = new Subject().setName("S");
 
-    rabbitOrKafka_Send(teacher);
+//    rabbitOrKafka_Send(teacher);
     // incorrect data sent to remote system in a non-transactable way
 
 //    subject.setHolderTeacher(teacher);
 
     teacher.addSubject(subject); // does not get persisted. but if you leave it out, later in a complex flow
 
+    eventPublisher.publishEvent(new TeacherCreatedEvent(teacher));
 //     your (JAVA OBJECTS) model remains INVALID
 //    subject.setHolderTeacher(teacher); // [THE OWNER side] only one of the ends is saved to DB
 
@@ -46,7 +48,15 @@ private final SubjectRepo subjectRepo;
     log.info("END");
   }
 
-  private void rabbitOrKafka_Send(Teacher teacher) {
-    System.out.println("SEND " + teacher);
+  private final ApplicationEventPublisher eventPublisher;
+  @Value
+  public static class TeacherCreatedEvent {
+    Teacher teacher;
+  }
+
+//  @EventListener
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  public void rabbitOrKafka_Send(TeacherCreatedEvent event) {
+    System.out.println("SEND " + event.teacher);
   }
 }
