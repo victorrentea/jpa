74c1f0f6b7c795dc8170dfeada62a413cd9c3171
Victor Rentea <victorrentea@gmail.com>
2025-10-22 12:29:17 +0300

    3 salveaza in DB ordinea manual setata de user in UI cu 🔼/🔽

--- src/main/java/victor/training/jpa/app/entity/Teacher.java
+++ src/main/java/victor/training/jpa/app/entity/Teacher.java
@@ -41,9 +41,9 @@ public class Teacher {
 	private MoreTeacherDetails moreDetails;
 	
 	@ElementCollection
-//	@OrderColumn(name="INDEX")
-	@OrderBy("type ASC, value ASC") // ti le sorteaza din query SQL trimis de JPA in DB
-	private List<ContactChannel> channels = new ArrayList<>();
+	@OrderColumn(name="INDEX") // 3 salveaza in DB ordinea manual setata de user in UI cu 🔼/🔽
+//	@OrderBy("type ASC, value ASC") // 2 ti le sorteaza din query SQL trimis de JPA in DB
+	private List<ContactChannel> channels = new ArrayList<>(); // 1 fara nimic, ordinea nu e garantata
 
 	@OneToMany(mappedBy = "holderTeacher")
 	private Set<Subject> heldSubjects = new HashSet<>() ;
