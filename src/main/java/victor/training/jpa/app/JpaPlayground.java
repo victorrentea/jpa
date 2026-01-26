465d5584b18b87b5b54c6fa76a4be8f8b3b91fbf
Victor Rentea <victorrentea@gmail.com>
2026-01-26 14:41:49 +0200

    fluent construction - și vei începe să-ți iubești, din nou, testele.

--- src/main/java/victor/training/jpa/app/JpaPlayground.java
+++ src/main/java/victor/training/jpa/app/JpaPlayground.java
@@ -1,49 +1,37 @@
 package victor.training.jpa.app;
 
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import victor.training.jpa.app.entity.Teacher;
 import victor.training.jpa.app.entity.TeacherDetails;
-import victor.training.jpa.app.repo.TeacherDetailsRepo;
 import victor.training.jpa.app.repo.TeacherRepo;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class JpaPlayground {
-   private final TeacherRepo teacherRepo;
+  private final TeacherRepo teacherRepo;
 
-   @Transactional
-   public void play() throws Exception {
-      Teacher teacher = new Teacher();
-      teacher.setName("John Doe");
-      TeacherDetails teacherDetails = new TeacherDetails()
-          .setCv("BETON");
-      teacher.setDetails(teacherDetails);
-      log.info("Before save: {}", teacher.getId());
-      teacherRepo.save(teacher); // (A)
+  @Transactional
+  public void play() throws Exception {
+    Teacher teacher = new Teacher()
+        .setName("John Doe")
+        .setDetails(new TeacherDetails()
+            .setCv("BETON"));
+    log.info("Before save: {}", teacher.getId());
+    teacherRepo.save(teacher); // (A)
 
-      System.out.println("E? " +  teacherRepo.findById(teacher.getId()));
-      log.info("After save: {}", teacher.getId());
-   }
-   // (B)
+    System.out.println("E? " + teacherRepo.findById(teacher.getId()));
+    log.info("After save: {}", teacher.getId());
+  }
+  // (B)
 
-   // 2 x INSERT intr-o tx❤️ pleaca in DB la .save (A) = asteptat
+  // 2 x INSERT intr-o tx❤️ pleaca in DB la .save (A) = asteptat
 
-   // 2 x INSERT intr-o tx❤️ pleaca in DB dupa iesirea din metoda @Transactional(B) 😱😱
-   // daca metoda e @Transactional = WRITE-BEHIND
-   // 😊 performance pt ca inserturi catre aceeasi entitate pot fi batch-euite impreuna sa TCP/IP mai putin
-   // 🙁
+  // 2 x INSERT intr-o tx❤️ pleaca in DB dupa iesirea din metoda @Transactional(B) 😱😱
+  // daca metoda e @Transactional = WRITE-BEHIND
+  // 😊 performance pt ca inserturi catre aceeasi entitate pot fi batch-euite impreuna sa TCP/IP mai putin
+  // 🙁
 }
